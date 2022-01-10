f4bff743cd507e798cebd98a1e354fc62a068c14
abc <abc@example.com>
2022-01-10 15:49:00 -0700

    I finished the post and 2 gets

--- api/server.js
+++ api/server.js
@@ -1,3 +1,56 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const Member = require('./users/model.js');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+//instance of express app
+const server = express();
+
+// global middleware
+server.use(express.json())
+
+//end points
+
+// [post]
+server.post('/api/users', async (req, res) => {
+    try {
+        const { name, bio } = req.body
+        // console.log(id, name, bio)
+        const newMember = await Member.insert({ name, bio})
+        console.log(newMember)
+        res.status(201).json(newMember)
+    } catch(err) {
+        res.status(500).json({ message: + err.message })
+    }
+});
+
+// [get]
+server.get('/api/users', async (req, res) => {
+    try {
+        const friend = await Member.find()
+        res.json(friend)
+    } catch (err) {
+        res.status(500).json({message: err.message})
+    }
+})
+// [get]
+server.get('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        console.log(id)
+        const friend = await Member.findById(id)
+        if (!friend){
+            res.status(404).json({ message: 'no member'})
+        } else {
+            res.status(200).json(friend)
+        }
+    } catch (err) {
+        res.status(500).json({message: err.message})
+    }
+})
+
+// [delete]
+server.delete('/api/users/:id')
+// [put]
+server.put('/api/users/:id')
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
